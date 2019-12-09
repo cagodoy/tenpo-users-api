e561addfd42f391a10db878542575f1983913c52
Camilo <cagodoy@acid.cl>
2019-12-09 17:18:48 -0300

    add dabase folder with respective migrations and store implemented methods

---
+++ database/database.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	users "github.com/cagodoy/tenpo-users-api"
+	"github.com/cagodoy/tenpo-users-api/database/postgres"
+	"github.com/jmoiron/sqlx"
+)
+
+// Store ...
+type Store interface {
+	Get(*users.Query) (*users.User, error)
+	Create(*users.User) error
+
+	// TODO(ca): below methods are not implemented
+	// List() ([]*users.User, error)
+	// Update(*users.User) error
+	// Delete(*users.User) error
+}
+
+// NewPostgres ...
+func NewPostgres(dsn string) (Store, error) {
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return &postgres.UserStore{
+		Store: db,
+	}, nil
+}
---
+++ database/postgres/users.go
@@ -0,0 +1,68 @@
+package postgres
+
+import (
+	"errors"
+
+	"github.com/Masterminds/squirrel"
+	users "github.com/cagodoy/tenpo-users-api"
+	"github.com/jmoiron/sqlx"
+)
+
+// UserStore ...
+type UserStore struct {
+	Store *sqlx.DB
+}
+
+// Get ...
+func (us *UserStore) Get(q *users.Query) (*users.User, error) {
+	query := squirrel.Select("*").From("users").Where("deleted_at is null")
+
+	if q.ID == "" && q.Email == "" {
+		return nil, errors.New("must proovide a query")
+	}
+
+	if q.ID != "" {
+		query = query.Where("id = ?", q.ID)
+	}
+
+	if q.Email != "" {
+		query = query.Where("email = ?", q.Email)
+	}
+
+	sql, args, err := query.PlaceholderFormat(squirrel.Dollar).ToSql()
+	if err != nil {
+		return nil, err
+	}
+
+	row := us.Store.QueryRowx(sql, args...)
+
+	c := &users.User{}
+	if err := row.StructScan(c); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+// Create ...
+func (us *UserStore) Create(u *users.User) error {
+
+	sql, args, err := squirrel.
+		Insert("users").
+		Columns("email", "name", "password").
+		Values(u.Email, u.Name, u.Password).
+		Suffix("returning *").
+		PlaceholderFormat(squirrel.Dollar).
+		ToSql()
+
+	if err != nil {
+		return err
+	}
+
+	row := us.Store.QueryRowx(sql, args...)
+	if err := row.StructScan(u); err != nil {
+		return err
+	}
+
+	return nil
+}
